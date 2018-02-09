d26f1f2170568e1562fbf5a5d5d8f2be7fe601c3
AlexKrylov <33526162+AlexKrylov@users.noreply.github.com>
2018-02-10 00:16:58 +0300

    Disable csrf

---
+++ src/main/java/ru/dbox/controller/UserController.java
@@ -0,0 +1,13 @@
+package ru.dbox.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.dbox.service.UserService;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserService userService;
+
+
+}
--- src/main/java/ru/dbox/security/SecurityConfig.java
+++ src/main/java/ru/dbox/security/SecurityConfig.java
@@ -1,7 +1,7 @@
 package ru.dbox.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling().accessDeniedPage("/403")
                 .and()
-                .csrf();
+                .csrf()
+                .disable();
     }
 
     @Autowired
